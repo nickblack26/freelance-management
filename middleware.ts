550c06b6bbb6218d8c509333738d53881ea0e984
Nick Black <49686365+nickblack26@users.noreply.github.com>
2023-08-08 14:10:13 -0500

    gets path and user and redirects properly

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,32 @@
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
-import { NextResponse } from 'next/server';
-
-import type { NextRequest } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 
 export async function middleware(req: NextRequest) {
 	const res = NextResponse.next();
 	const supabase = createMiddlewareClient<Database>({ req, res });
 
-	const user = await supabase.auth.getUser();
-
-	// console.log(user, req.nextUrl.pathname);
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
 
 	// if user is signed in and the current path is / redirect the user to /account
-	if (user && req.nextUrl.pathname === '/[org_id]/') {
-		return NextResponse.redirect(new URL('/[org_id]/', req.url));
+	if (user && req.nextUrl.pathname === '/') {
+		return NextResponse.redirect(new URL(`/${user.user_metadata.organizations[0]}`, req.url));
+	}
+
+	// if user is not signed in and the current path is / redirect the user to /login
+	if (user && req.nextUrl.pathname === '/') {
+		return NextResponse.redirect(new URL('/login', req.url));
 	}
 
 	// if user is not signed in and the current path is not / redirect the user to /
 	if (!user && req.nextUrl.pathname !== '/') {
-		return NextResponse.redirect(new URL('/login', req.url));
+		return NextResponse.redirect(new URL('/', req.url));
 	}
 
 	return res;
 }
 
 export const config = {
-	matcher: '/[org_id]/:path*',
+	matcher: '/',
 };
