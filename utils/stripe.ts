178468bcc4b34a232b47c2d5a268deaf32b63a8a
Nick Black <49686365+nickblack26@users.noreply.github.com>
2024-04-03 14:58:13 -0500

    Delete utils directory

--- utils/stripe.ts
+++
@@ -1,6 +0,0 @@
-import { loadStripe } from '@stripe/stripe-js';
-import Stripe from 'stripe';
-
-export const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY!, {
-	apiVersion: '2022-11-15',
-});
